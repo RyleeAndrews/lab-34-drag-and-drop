8c5b1b8d474b19cc2e3421d168f67bb526052662
Rylee Andrews <thebinarywithextrasauce@Rylees-MacBook-Pro.local>
2018-01-17 19:30:30 -0800

    added drag and drop working on updating the expense

---
+++ src/component/drag-n-drop/drop.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {activeKeys} from '../../lib/__';
+
+class DropZone extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {droppable:false}
+
+        this.handleDrop = this.handleDrop.bind(this);
+        this.handleDragOver = this.handleDragOver.bind(this);
+
+    }
+
+    handleDragOver(e) {
+        e.preventDefault();
+        this.setState({droppable:true});
+    }
+
+    handleDrop(e) {
+        e.preventDefault();
+        let payload = e.dataTransfer.getData("text/json");
+        console.log('yoyoyio', e.dataTransfer);
+        let expense = JSON.parse(payload);
+        this.props.onComplete(expense);
+        this.setState({droppable:false});
+    }
+
+    render() {
+
+        let classConfig = {
+            [this.props.containerClass]: true,
+            droppable: this.state.droppable
+        }
+
+        let classes = activeKeys(classConfig).join(" ");
+
+        return (
+            <div
+                className={classes}
+                onDrop={this.handleDrop}
+                onDragOver={this.handleDragOver}
+            >
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default DropZone;
