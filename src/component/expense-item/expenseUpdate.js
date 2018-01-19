a6b9fa1da0fcb3c1303bda823700f1493e03b34a
Rylee Andrews <thebinarywithextrasauce@Rylees-MacBook-Pro.local>
2018-01-18 21:09:52 -0800

    budget now subtracting from expenses

---
+++ src/component/expense-item/expenseUpdate.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class UpdateExpense extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.onChangeOfName = this.onChangeOfName.bind(this);
+    this.onChangeOfCost = this.onChangeOfCost.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      ...props.expense
+    }
+  }
+
+  onChangeOfName(event){
+    event.preventDefault();
+
+    this.setState({name: event.target.value});
+  }
+
+  onChangeOfCost(event){
+    event.preventDefault();
+    this.setState({cost: event.target.value});
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    let newState = {...this.state}
+    this.props.expenseUpdate(newState);
+  }
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.onSubmit}>
+          Edit your expense name
+          <input value={this.state.name} onChange={this.onChangeOfName}/>
+          <br />
+          <input value={this.state.cost} onChange={this.onChangeOfCost}/>
+          <button type="submit"> submit </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UpdateExpense;
--- src/component/expense-item/index.js
+++ src/component/expense-item/index.js
@@ -1,18 +1,34 @@
 import './expense-item.scss';
 import React from 'react';
+import Draggable from '../drag-n-drop/drag.js';
+import UpdateExpense from './expenseUpdate.js';
 
 class ExpenseItem extends React.Component{
   constructor(props){
     super(props);
+
+    this.handleDragStart = this.handleDrag.bind(this);
   }
+
+  handleDrag(e){
+    let expense = this.props.expenses[this.props.categoryID].filter(item => {
+      return item.id === e.target.id;})[0];
+    e.dataTransfer.setData('text/json', JSON.stringify(expense));
+  }
+
+
 
   render(){
     return(
       <div className='expense-item'>
-        {this.props.expenses[this.props.categoryID].map((expense,i) => 
-          <div key={expense.id}>
+        {this.props.expenses[this.props.categoryID].map((expense,i) =>
+          <div key={expense.id} id={expense.id}>
+          <UpdateExpense expenseUpdate={this.props.expenseUpdate} expense={expense}/>
+          <Draggable onDragStart={this.handleDrag} expense={expense} draggable="true">
             <p> {(expense.name)} </p>
+            <p> {expense.cost} </p>
             <button onClick={() => this.props.expenseDelete(expense)}> x </button>
+          </Draggable>
           </div>
         )}
       </div>
